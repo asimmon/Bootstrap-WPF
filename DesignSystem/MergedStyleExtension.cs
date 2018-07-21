8b898924154a1e99f962098769b6b2dd8f8bb997
Anthony Simmon <anthony.simmon@gsoft.com>
2018-07-21 01:26:47 -0400

    Amélioration du MergedStyle avec un mode inline

--- DesignSystem/MergedStyleExtension.cs
+++ DesignSystem/MergedStyleExtension.cs
@@ -19,7 +19,30 @@ namespace DesignSystem
             get { return (IList)this._styles; }
         }
 
+        public Style Style1 { get; set; }
+        public Style Style2 { get; set; }
+        public Style Style3 { get; set; }
+        public Style Style4 { get; set; }
+        public Style Style5 { get; set; }
+
         public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            AddStyleIfNotNull(Style1);
+            AddStyleIfNotNull(Style2);
+            AddStyleIfNotNull(Style3);
+            AddStyleIfNotNull(Style4);
+            AddStyleIfNotNull(Style5);
+
+            return MergeStyles();
+        }
+
+        private void AddStyleIfNotNull(Style style)
+        {
+            if (style != null)
+                _styles.Add(style);
+        }
+
+        private object MergeStyles()
         {
             var mergedStyle = new Style();
 
