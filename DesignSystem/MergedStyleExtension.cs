5cfcc815a6ba358679ac183b36d848a5f3a1fd0b
Anthony Simmon <anthony.simmon@gsoft.com>
2018-07-21 01:11:36 -0400

    Création et utilisation de MergedStyle

---
+++ DesignSystem/MergedStyleExtension.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Markup;
+
+namespace DesignSystem
+{
+    [ContentProperty("Styles")]
+    [MarkupExtensionReturnType(typeof(Style))]
+    public class MergedStyleExtension : MarkupExtension
+    {
+        private readonly IList<Style> _styles = new List<Style>();
+
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
+        public IList Styles
+        {
+            get { return (IList)this._styles; }
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            var mergedStyle = new Style();
+
+            foreach (var style in _styles)
+                mergedStyle.Merge(style);
+
+            return mergedStyle;
+        }
+    }
+}
