25240054cb76754555ea02918071d4e97cfeccb5
Anthony Simmon <anthony.simmon@gsoft.com>
2018-07-28 19:05:21 -0400

    Added textbox watermark and textbox inset shadow

---
+++ DesignSystem/WatermarkAdorner.cs
@@ -0,0 +1,98 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace DesignSystem
+{
+    internal class WatermarkAdorner : Adorner
+    {
+        public static readonly DependencyProperty IsOverflowingAdornedElementProperty = DependencyProperty.Register(
+            "IsOverflowingAdornedElement",
+            typeof(bool),
+            typeof(WatermarkAdorner),
+            new PropertyMetadata(default(bool)));
+
+        private readonly ContentPresenter _contentPresenter;
+
+        public WatermarkAdorner(UIElement adornedElement, object watermark, DataTemplate watermarkTemplate)
+            : base(adornedElement)
+        {
+            IsHitTestVisible = false;
+
+            var template = watermarkTemplate ??
+                           Control.TryFindResource("WatermarkPresenter_" + adornedElement.GetType().Name) as DataTemplate ??
+                           Control.TryFindResource("WatermarkPresenter") as DataTemplate;
+
+            if (template != null)
+            {
+                _contentPresenter = (ContentPresenter)template.LoadContent();
+            }
+            else
+            {
+                _contentPresenter = new ContentPresenter();
+            }
+
+            // set the content
+            _contentPresenter.Content = watermark;
+
+            if (Control is ItemsControl && !(Control is ComboBox))
+            {
+                _contentPresenter.VerticalAlignment = VerticalAlignment.Center;
+                _contentPresenter.HorizontalAlignment = HorizontalAlignment.Center;
+            }
+
+            var binding = new Binding(IsVisibleProperty.Name);
+            binding.Source = adornedElement;
+            binding.Converter = new BooleanToVisibilityConverter();
+            SetBinding(VisibilityProperty, binding);
+        }
+
+        public bool IsOverflowingAdornedElement
+        {
+            get { return (bool)GetValue(IsOverflowingAdornedElementProperty); }
+            set { SetValue(IsOverflowingAdornedElementProperty, value); }
+        }
+
+        protected override int VisualChildrenCount
+        {
+            get { return 1; }
+        }
+
+        private Control Control
+        {
+            get { return (Control) AdornedElement; }
+        }
+
+        /// When overridden in a derived class, positions child elements and determines a size for a <see cref="FrameworkElement"/> derived class.
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            _contentPresenter.Arrange(new Rect(finalSize));
+            return finalSize;
+        }
+
+        /// Returns a specified child <see cref="Visual"/> for the parent <see cref="ContainerVisual"/>.
+        protected override Visual GetVisualChild(int index)
+        {
+            return _contentPresenter;
+        }
+
+        /// Implements any custom measuring behavior for the adorner.
+        protected override Size MeasureOverride(Size constraint)
+        {
+            // First, we calculate the actual desired size of the watermark content within the adorner
+            _contentPresenter.Measure(constraint);
+
+            var desiredSize = _contentPresenter.DesiredSize;
+            var actualSize = Control.RenderSize;
+
+            // Here's the secret to getting the adorner to cover the whole control
+            _contentPresenter.Measure(Control.RenderSize);
+
+            // To check if the watermark overflows the control, we compare its width.
+            IsOverflowingAdornedElement = desiredSize.Width > actualSize.Width;
+            return actualSize;
+        }
+    }
+}
---
+++ DesignSystem/WatermarkService.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Documents;
+
+namespace DesignSystem
+{
+    public static class WatermarkService
+    {
+        public static readonly DependencyProperty WatermarkProperty = DependencyProperty.RegisterAttached(
+           "Watermark",
+           typeof(object),
+           typeof(WatermarkService),
+           new FrameworkPropertyMetadata(null, OnWatermarkChanged));
+
+        public static readonly DependencyProperty WatermarkTargetProperty = DependencyProperty.RegisterAttached(
+           "WatermarkTarget",
+           typeof(UIElement),
+           typeof(WatermarkService),
+           new FrameworkPropertyMetadata(null, OnWatermarkTargetChanged));
+
+        public static readonly DependencyProperty WatermarkDataTemplateProperty = DependencyProperty.RegisterAttached(
+           "WatermarkDataTemplate",
+           typeof(DataTemplate),
+           typeof(WatermarkService),
+           new FrameworkPropertyMetadata((object)null, null));
+
+        public static void OnWatermarkedControlChanged(object sender, EventArgs e)
+        {
+            var control = (Control)sender;
+
+            if (ShouldShowWatermark(control))
+            {
+                ShowWatermark(control, GetWatermarkTarget(control) ?? control);
+            }
+            else
+            {
+                RemoveWatermark(GetWatermarkTarget(control) ?? control);
+            }
+        }
+
+        public static object GetWatermark(DependencyObject d)
+        {
+            return d.GetValue(WatermarkProperty);
+        }
+
+        public static void SetWatermark(DependencyObject d, object value)
+        {
+            d.SetValue(WatermarkProperty, value);
+        }
+
+        public static DataTemplate GetWatermarkDataTemplate(DependencyObject d)
+        {
+            return (DataTemplate)d.GetValue(WatermarkDataTemplateProperty);
+        }
+
+        public static void SetWatermarkDataTemplate(DependencyObject d, DataTemplate value)
+        {
+            d.SetValue(WatermarkDataTemplateProperty, value);
+        }
+
+        public static UIElement GetWatermarkTarget(DependencyObject d)
+        {
+            return (UIElement)d.GetValue(WatermarkTargetProperty);
+        }
+
+        public static void SetWatermarkTarget(DependencyObject d, UIElement value)
+        {
+            d.SetValue(WatermarkTargetProperty, value);
+        }
+
+        public static void OnWatermarkChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var control = (Control)d;
+            control.Loaded += OnWatermarkedControlChanged;
+
+            PasswordBox passwordBox;
+            TextBoxBase textBox;
+
+            if ((passwordBox = d as PasswordBox) != null)
+            {
+                passwordBox.PasswordChanged += OnWatermarkedControlChanged;
+            }
+            else if ((textBox = d as TextBoxBase) != null)
+            {
+                textBox.TextChanged += OnWatermarkedControlChanged;
+            }
+        }
+
+        private static void OnWatermarkTargetChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            RemoveWatermark((e.OldValue as UIElement) ?? (Control)d);
+            OnWatermarkedControlChanged(d, EventArgs.Empty);
+        }
+
+        private static void RemoveWatermark(UIElement control)
+        {
+            var layer = AdornerLayer.GetAdornerLayer(control);
+            if (layer != null)
+            {
+                RemoveAllWatermarksFromLayer(control, layer);
+            }
+        }
+
+        private static void ShowWatermark(UIElement control, UIElement adornedControl)
+        {
+            var layer = AdornerLayer.GetAdornerLayer(adornedControl);
+            if (layer != null)
+            {
+                RemoveAllWatermarksFromLayer(adornedControl, layer);
+
+                layer.Add(new WatermarkAdorner(adornedControl, GetWatermark(control), GetWatermarkDataTemplate(control)));
+            }
+        }
+
+        private static void RemoveAllWatermarksFromLayer(UIElement control, AdornerLayer layer)
+        {
+            var adorners = layer.GetAdorners(control);
+            if (adorners != null && adorners.Length > 0)
+            {
+                foreach (var adorner in adorners.OfType<WatermarkAdorner>())
+                {
+                    adorner.Visibility = Visibility.Hidden;
+                    layer.Remove(adorner);
+                }
+            }
+        }
+
+        private static bool ShouldShowWatermark(Control c)
+        {
+            PasswordBox passwordBox;
+            TextBox textBox;
+
+            if ((passwordBox = c as PasswordBox) != null)
+            {
+                return string.IsNullOrEmpty(passwordBox.Password);
+            }
+            else if ((textBox = c as TextBox) != null)
+            {
+                return string.IsNullOrEmpty(textBox.Text);
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
